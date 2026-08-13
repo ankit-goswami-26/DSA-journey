1e2c3c0b0d315a5477415064c244eeed0a1ef447
Ankit Goswami <goswamiankit459@gmail.com>
2026-08-13 21:56:07 +0530

    Time: 1 ms (86.74%), Space: 42.9 MB (23.63%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -4,10 +4,8 @@ class Solution {
         int count = 0;
 
         for(int i = 0; i < nums.length; i++){
-            for(int j = 1; j < nums.length; j++){
-                if(nums[i] == nums[j] && i < j){
-                    count++;
-                }
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]) count++;
             }
         }
 
