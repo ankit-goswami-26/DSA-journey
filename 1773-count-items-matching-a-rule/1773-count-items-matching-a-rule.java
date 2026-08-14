8e312cd850d0316e2997573e9e02f5a8d4038a8e
Ankit Goswami <goswamiankit459@gmail.com>
2026-08-14 07:28:23 +0530

    Time: 6 ms (28.28%), Space: 49.7 MB (28.81%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -4,15 +4,10 @@ class Solution {
 
         for(int i = 0; i < items.size(); i++){
 
-            if(ruleKey.equals("type") && ruleValue.equals(items.get(i).get(0))){
-                match++;
-            } 
-            else if(ruleKey.equals("color") && ruleValue.equals(items.get(i).get(1))){
-                match++;
-            } 
-            else if(ruleKey.equals("name") && ruleValue.equals(items.get(i).get(2))){
-                match++;
-            }
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue))match++;
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue))match++;
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue))match++;
+
         }
 
         return match;
